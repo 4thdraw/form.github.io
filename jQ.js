bc6749f4bdd4d0206315ca1c1d4874762e447dac
BoYeungBang <57360625+4thdraw@users.noreply.github.com>
2023-06-19 17:08:24 +0900

    전체버튼 클릭시 나머지 체크박스 선택 및 해제

--- jQ.js
+++ jQ.js
@@ -1,11 +1,22 @@
 // const bbyjQ = jQuery.noConflict();
 
 $(function(){
-$("#emailsection .form-select").change(function(){
-             //console.log($(this).val())
-if( $(this).val() == '직접입력' || $(this).val() == '' ){
-    $("#emailsection").addClass("act");
-    $("#emailsection .directW").focus();
-}
+    $("#emailsection .form-select").change(function(){
+            // console.log($(this).val())
+        if( $(this).val() == '직접입력' || $(this).val() == '' ){
+            $("#emailsection").addClass("act");
+            $("#emailsection .directW").focus();
+        }else{
+            $("#emailsection").removeClass("act");
+        }
+    })
+    $("#lenSection input").change(function(){
+        //console.log($(this).val())
+        if( $("#lengall").prop('checked') ){ // $("#lengall").is(':checked')
+            $("#lenSection input:checkbox").prop('checked',true) 
+        }else{
+            $("#lenSection input:checkbox").prop('checked',false) 
+        }
+        //all을 제외한 나머지 checkbox가 checked라면 all을 체크해라.
     })
 })
